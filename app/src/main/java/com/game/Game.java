9ea94ecf6fb8a3d23bbc93abffbb0e9c833fbd95
Gliiin <Dmitriy.s2006@yandex.ru>
2023-06-01 16:54:52 +0400

    game fixed v.3

--- app/src/main/java/com/game/Game.java
+++ app/src/main/java/com/game/Game.java
@@ -1,18 +1,24 @@
 package com.game;
 
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 
 public class Game extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +27,17 @@ public class Game extends AppCompatActivity {
         setContentView(new GamePanel(this));
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         ImageButton button_right = new ImageButton(this);
-        button_right.setImageResource(R.drawable.button_right);
-        button_right.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        addContentView(button_right, new FrameLayout.LayoutParams(100, 100));
-        //button_right.
-
+        ImageButton button_left = new ImageButton(this);
+        ConstraintLayout button_layout = new ConstraintLayout(this);
+        button_right.setImageBitmap(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.button_right), 320, 240, false));
+        button_left.setImageBitmap(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.button_left), 320, 240, false));
+        button_right.setBackgroundResource(0);
+        button_left.setBackgroundResource(0);
+        button_layout.addView(button_left);
+        button_layout.addView(button_right);
+        button_layout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        addContentView(button_layout, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        System.out.println();
     }
 
 }
